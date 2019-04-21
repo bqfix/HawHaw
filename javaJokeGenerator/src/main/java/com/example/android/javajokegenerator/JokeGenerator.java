d158bcaee3ab39b6675589316e7dede175c71b42
bqfix <benjaminfix@gmail.com>
2019-04-21 16:25:24 -0400

    refactor: Added jokes
    Changed jokes from generic placeholders to a selection of puns from my lovely associates.

--- javaJokeGenerator/src/main/java/com/example/android/javajokegenerator/JokeGenerator.java
+++ javaJokeGenerator/src/main/java/com/example/android/javajokegenerator/JokeGenerator.java
@@ -6,9 +6,14 @@ import java.util.Arrays;
 public class JokeGenerator {
     private ArrayList<String> jokes =  new ArrayList<>(
             Arrays.asList(
-                    "This is joke #1",
-                    "This is joke #2",
-                    "This is joke #3"
+                    "What is red and smells like blue paint?\n\nRed paint.",
+                    "What do you call a fish with no eyes?\n\nA fsh.",
+                    "What has 4 letters, sometimes has 9 letters, but never has 5 letters.",
+                    "What's a pirate's favorite letter?\n\nArrr.",
+                    "What computer sings the best?\n\nA Dell.",
+                    "How do you find Will Smith in the snow?\n\nLook for the fresh prints.",
+                    "What's a dentist's favorite time of day?\n\nTooth hurty.",
+                    "How many software engineers does it take to screw in a lightbulb?\n\nNone, it's a hardware issue."
             ));
 
     public String getJoke(){
